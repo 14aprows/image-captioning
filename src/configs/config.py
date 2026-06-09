e411586c484061e580a0dd4725535f55512b637b
lwkys <Hasibuanfirman1404@gmail.com>
2026-06-09 23:42:58 +0700

    add caption vocab config

--- src/configs/config.py
+++ src/configs/config.py
@@ -9,3 +9,10 @@ TEXT_DIR = DATA_DIR / "visual_text"
 TOKEN_FILE = TEXT_DIR / "visual.token.txt"
 TRAIN_IMAGE_FILE = TEXT_DIR / "visual.trainImages.txt"
 TEST_IMAGE_FILE = TEXT_DIR / "visual.testImages.txt"
+
+PAD_TOKEN = "<pad>"
+START_TOKEN = "<start>"
+END_TOKEN = "<end>"
+UNK_TOKEN = "<unk>"
+
+MIN_WORD_FREQ = 1
